954067f3d7d8e08fa079ae19783d451f807aa282
ramil_novruzov <ramil.novruzov@gmx.de>
2025-05-06 09:05:59 +0200

    added cloudinary

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Укажите имя вашего Cloudinary аккаунта
-  api_key: process.env.CLOUDINARY_API_KEY, // Ваш API ключ
-  api_secret: process.env.CLOUDINARY_API_SECRET, // Ваш секретный ключ
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
