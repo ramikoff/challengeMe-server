ada03496a2bd961009fd849cb9e64d3d6e7df8e1
ramil_novruzov <ramil.novruzov@gmx.de>
2025-05-14 11:40:03 +0200

    cleaning

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -25,10 +25,14 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-const upload = multer({
-  storage,
-  fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 },
-});
+// const upload = multer({
+//   storage,
+//   fileFilter,
+//   limits: { fileSize: 5 * 1024 * 1024 },
+// });
+
+const fileSize = 5 * 1024 * 1024; // 5mb
+
+const upload = multer({ storage, fileFilter, limits: { fileSize } });
 
 export default upload;
